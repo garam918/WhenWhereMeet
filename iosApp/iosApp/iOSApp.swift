ab8799cb2c04131ca2f5631b3964a99fd1b0fc72
imgaram <ijy918@naver.com>
2026-06-19 11:19:04 +0900

    Wire iOS app dependencies

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
+import FirebaseCore
 
 @main
 struct iOSApp: App {
+    init() {
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
