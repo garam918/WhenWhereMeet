e9f9c59c4a85705041a9574171fdb890fbd6b795
imgaram <ijy918@naver.com>
2026-06-19 11:19:04 +0900

    Add storage clear contract

--- shared/src/commonMain/kotlin/com/garam/whenwheremeet/data/local/KeyValueStorage.kt
+++ shared/src/commonMain/kotlin/com/garam/whenwheremeet/data/local/KeyValueStorage.kt
@@ -3,6 +3,7 @@ package com.garam.whenwheremeet.data.local
 interface KeyValueStorage {
     fun getString(key: String): String?
     fun putString(key: String, value: String)
+    fun remove(key: String)
 }
 
 expect fun platformKeyValueStorage(): KeyValueStorage
