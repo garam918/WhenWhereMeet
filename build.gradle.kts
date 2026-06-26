39e94ce2c1a1d6fd82ea2c36e585fd3a9e2b9a89
imgaram <ijy918@naver.com>
2026-06-26 14:28:32 +0900

    Register Firebase Crashlytics plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.firebaseCrashlytics) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.androidxRoom) apply false
 }
