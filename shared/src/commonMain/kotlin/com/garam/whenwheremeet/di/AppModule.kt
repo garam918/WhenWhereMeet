377fa82e802b377340bdb6bff5fe7e94b2b3c08c
imgaram <ijy918@naver.com>
2026-06-26 14:28:33 +0900

    Add shared dependency injection module

---
+++ shared/src/commonMain/kotlin/com/garam/whenwheremeet/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.garam.whenwheremeet.di
+
+import com.garam.whenwheremeet.createMeetingRepository
+import com.garam.whenwheremeet.data.local.KeyValueStorage
+import com.garam.whenwheremeet.data.local.platformKeyValueStorage
+import com.garam.whenwheremeet.data.provider.FakeLocationSearchProvider
+import com.garam.whenwheremeet.data.provider.FakePlaceSearchProvider
+import com.garam.whenwheremeet.data.provider.MetropolitanMeetingAreaCandidateProvider
+import com.garam.whenwheremeet.data.provider.StaticMetropolitanTransitTimeProvider
+import com.garam.whenwheremeet.data.repository.LocalMeetingRepository
+import com.garam.whenwheremeet.domain.provider.LocationSearchProvider
+import com.garam.whenwheremeet.domain.provider.PlaceSearchProvider
+import com.garam.whenwheremeet.domain.provider.TravelTimeProvider
+import com.garam.whenwheremeet.domain.repository.MeetingRepository
+import com.garam.whenwheremeet.domain.usecase.RecommendMeetingAreasUseCase
+import com.garam.whenwheremeet.presentation.state.MeetingAppState
+import org.koin.dsl.module
+
+val appModule = module {
+    single<KeyValueStorage> { platformKeyValueStorage() }
+    single { LocalMeetingRepository(get()) }
+    single<MeetingRepository> { createMeetingRepository(get()) }
+
+    single<LocationSearchProvider> { FakeLocationSearchProvider() }
+    single<TravelTimeProvider> { StaticMetropolitanTransitTimeProvider() }
+    single { RecommendMeetingAreasUseCase(get()) }
+    single<PlaceSearchProvider> { FakePlaceSearchProvider() }
+    single { MetropolitanMeetingAreaCandidateProvider() }
+
+    factory {
+        MeetingAppState(
+            repository = get(),
+            locationSearchProvider = get(),
+            recommendMeetingAreas = get(),
+            placeSearchProvider = get(),
+        )
+    }
+}
