5ab55a357b1b0c45b6c0d18b43dc77aadf732780
imgaram <ijy918@naver.com>
2026-06-26 14:28:33 +0900

    Add web app entry point

---
+++ shared/src/webMain/kotlin/com/garam/whenwheremeet/Main.web.kt
@@ -0,0 +1,11 @@
+package com.garam.whenwheremeet
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport(viewportContainerId = "app") {
+        App()
+    }
+}
