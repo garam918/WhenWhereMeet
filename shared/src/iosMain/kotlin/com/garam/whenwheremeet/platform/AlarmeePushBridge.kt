c316894672f47373ab04cefb0afe9f2943a1bfc2
imgaram <ijy918@naver.com>
2026-08-19 00:11:43 +0900

    feat: iOS 알림 이벤트 Alarmee 브리지 추가

---
+++ shared/src/iosMain/kotlin/com/garam/whenwheremeet/platform/AlarmeePushBridge.kt
@@ -0,0 +1,21 @@
+package com.garam.whenwheremeet.platform
+
+import com.tweener.alarmee.PushNotificationServiceRegistry
+
+class AlarmeePushBridge {
+    fun onNewToken(token: String) {
+        PushNotificationServiceRegistry.notifyTokenUpdated(token)
+    }
+
+    fun onNotificationReceived(userInfo: Map<Any?, *>?) {
+        val data = userInfo
+            ?.mapNotNull { (key, value) ->
+                val normalizedKey = key?.toString()
+                val normalizedValue = value?.toString()
+                if (normalizedKey != null && normalizedValue != null) normalizedKey to normalizedValue else null
+            }
+            ?.toMap()
+            .orEmpty()
+        PushNotificationServiceRegistry.notifyIncomingMessage(data)
+    }
+}
